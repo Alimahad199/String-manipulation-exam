56e6986a994272041ed4f02a4b481ac476ae4f08
Ali <alimahad.eng@gmail.com>
2020-01-31 12:58:17 +0000

    first commit

---
+++ src/main/java/main/OfflineExercises.java
@@ -0,0 +1,202 @@
+package main;
+
+public class OfflineExercises {
+
+	// Given a string, return a string where
+	// for every char in the original string,
+	// there are three chars.
+
+	// multChar("The") → "TTThhheee"
+	// multChar("AAbb") → "AAAAAAbbbbbb"
+	// multChar("Hi-There") → "HHHiii---TTThhheeerrreee"
+
+	public String multChar(String input) {
+		String answer ="";
+		for(int i=0;i<input.length();i++){
+		for(int j=0;j<3;j++) {
+		answer= answer + input.charAt(i);
+			
+			}
+		}
+		
+		
+		return answer;
+        
+			
+	}
+	
+	// Return the string (backwards) that is between the first and last appearance
+	// of "bert"
+	// in the given string, or return the empty string "" if there is not 2
+	// occurances of "bert" - Ignore Case
+
+	// getBert("bertclivebert") → "evilc"
+	// getBert("xxbertfridgebertyy") → "egdirf"
+	// getBert("xxBertfridgebERtyy") → "egdirf"
+	// getBert("xxbertyy") → ""
+	// getBert("xxbeRTyy") → ""
+
+	public String getBert(String input) {
+		
+		
+		
+		return "";
+	}
+	
+
+	// Given three ints, a b c, one of them is small, one is medium and one is
+	// large. Return true if the three values are evenly spaced, so the
+	// difference between small and medium is the same as the difference between
+	// medium and large. Do not assume the ints will come to you in a reasonable
+	// order.
+
+	// evenlySpaced(2, 4, 6) → true
+	// evenlySpaced(4, 6, 2) → true
+	// evenlySpaced(4, 6, 3) → false
+	// evenlySpaced(4, 60, 9) → false
+
+	public boolean evenlySpaced(int a, int b, int c) {
+		
+		
+	}
+
+	// Given a string and an int n, return a string that removes n letters from the 'middle' of the string.
+	// The string length will be at least n, and be odd when the length of the input is odd.
+
+	// nMid("Hello", 3) → "Ho"
+	// nMid("Chocolate", 3) → "Choate"
+	// nMid("Chocolate", 1) → "Choclate"
+
+	public String nMid(String input, int a) {
+		return "";
+	}
+	
+	int mid input.length()/2;
+	int number 1 mid - (a/2);
+
+	int number 2 mid +(a+2);
+	string result="";
+	string sub = input.substring(number1,number2 +1);
+	if a>1{
+		result
+		sysout
+	}
+
+	// Given a string, return true if it ends in "dev". Ignore Case
+
+	// endsDev("ihatedev") → true
+	// endsDev("wehateDev") → true
+	// endsDev("everoyonehatesdevforreal") → false
+	// endsDev("devisnotcool") → false
+
+	public boolean endsDev(String input) {
+		
+	}
+		String name = input;
+		String tolower = name.toLowerCase();
+		if (tolower.endsWith("dev")) {
+		return true;
+		
+	}
+	else {
+		
+		return false;
+	
+	}
+	
+
+	// Given a string, return the length of the largest "block" in the string.
+	// A block is a run of adjacent chars that are the same.
+	//
+	// superBlock("hoopplla") → 2
+	// superBlock("abbCCCddDDDeeEEE") → 3
+	// superBlock("") → 0
+
+	public int superBlock(String input) {
+		return -1;
+
+	}
+	
+	//given a string - return the number of times "am" appears in the String ignoring case -
+	// BUT ONLY WHEN the word "am" appears without being followed or proceeded by other letters
+	//
+	//amISearch("Am I in Amsterdam") → 1
+	//amISearch("I am in Amsterdam am I?") → 2
+	//amISearch("I have been in Amsterdam") → 0
+
+	public int amISearch(String arg1) {
+		
+		int count=0;
+		String toLowerCase=arg1.toLowerCase();
+		String[]array= toLowerCase.split(" ");
+		for (String letter :array) {
+			if (letter.contentEquals("am")) {
+				count=count+1;
+			}
+			
+		}
+			return count;
+		
+			
+		}
+	
+		
+	
+		
+	
+	
+	//given a number 
+	// if this number is divisible by 3 return "fizz"
+	// if this number is divisible by 5 return "buzz"
+	// if this number is divisible by both 3  and 5return "fizzbuzz"
+	//
+	//fizzBuzz(3) → "fizz"
+	//fizzBuzz(10) → "buzz"
+	//fizzBuzz(15) → "fizzbuzz"
+	
+	public String fizzBuzz(int var1)
+		
+		String name= "";
+		if (var1%3==0) {
+			if(var1%5==0) {
+				name="fizzbuzz";
+		
+		}else {
+			name="fizz";
+			
+		}
+	}else if(var1%5==0) {
+			if(var1%3==0) {
+				name="fizzbuzz";
+			}else {
+				name="buzz";
+			}
+					
+		}
+		return name;
+		}
+	
+	
+	//Given a string split the string into the individual numbers present
+	//then add each digit of each number to get a final value for each number
+	// String example = "55 72 86"
+	//
+	// "55" will = the integer 10
+	// "72" will = the integer 9
+	// "86" will = the integer 14
+	//
+	// You then need to return the highest vale
+	//
+	//largest("55 72 86") → 14
+	//largest("15 72 80 164") → 11
+	//largest("555 72 86 45 10") → 15
+	
+	public int largest(String arg1) {
+		return 0;
+		
+	
+		
+	}
+	
+	
+	}
